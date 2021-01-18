28c1c9f54478a018c5190f8ca19d27f7c6daeb15
Evgeny Makhankov <EMakhankov@gmail.com>
2021-01-18 20:34:22 +0300

    Initial Commit

---
+++ IOS_Senin_3_ist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_Senin_3_ist
+//
+//  Created by Jenya on 18.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
