ba61b144e8f3130c245a507c568e97bb248024e0
Evgeny Makhankov <EMakhankov@gmail.com>
2021-01-20 21:56:05 +0300

    Button newFolder

---
+++ IOS_Senin_3_ist/Controllers/FoldersController.swift
@@ -0,0 +1,127 @@
+//
+//  FoldersController.swift
+//  IOS_Senin_3_ist
+//
+//  Created by Jenya on 20.01.2021.
+//
+
+import UIKit
+
+class FoldersController: UITableViewController {
+   
+    @IBAction func pushAddAction(_ sender: Any) {
+    
+        
+        
+        let alertController = UIAlertController(title: "Create new folder", message: "", preferredStyle: .alert);
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Folder name";
+        }
+        
+        let alertActionAdd = UIAlertAction(title: "Create", style: UIAlertAction.Style.default) { (alert) in
+            let folderName = alertController.textFields?[0].text
+            if folderName != "" {
+                _ = Folder.newFolder(name: folderName!)
+                CoreDataManager.sharedInstance.saveContext()
+                self.tableView.reloadData();
+            }
+        }
+
+        let alertActionCancel = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default) { (UIAlertAction) in
+                        
+        }
+        
+        alertController.addAction(alertActionAdd)
+        alertController.addAction(alertActionCancel)
+        
+        present(alertController, animated: true) {
+            
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return folders.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellFolder", for: indexPath)
+
+        // Configure the cell...
+        let folderInCell = folders[indexPath.row];
+        cell.textLabel?.text = folderInCell.name;
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
